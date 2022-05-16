31a124144459d2604b9b18cc578cf6ccefebf536
srharrison82 <srharrison82@gmail.com>
2022-05-16 15:20:55 +0100

    Added button

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log('Hello World');
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function() {
+    if (links.classList.contains('show-list')) {
+        links.classList.remove('show-list');
+    } else {
+        links.classList.add('show.links');
+    }
+});
